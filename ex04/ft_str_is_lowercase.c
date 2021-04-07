2893a07def84013d8aa2a49524be9ef34658df78
mmassaak <mmassaak@student.42sp.org.br>
2021-04-07 18:46:37 +0000

    add ex04

---
+++ ex04/ft_str_is_lowercase.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmassaak <mmassaak@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/07 00:02:48 by mmassaak          #+#    #+#             */
+/*   Updated: 2021/04/07 00:45:42 by mmassaak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_lowercase(char *str)
+{
+	int i;
+	int current_letter;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		current_letter = str[i];
+		if (current_letter < 'a' || current_letter > 'z')
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
