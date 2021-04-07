1db42c0242c02a1a9ad4d87834163963e40bbddf
mmassaak <mmassaak@student.42sp.org.br>
2021-04-07 18:46:53 +0000

    add ex06

---
+++ ex06/ft_str_is_printable.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_printable.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmassaak <mmassaak@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/07 00:02:48 by mmassaak          #+#    #+#             */
+/*   Updated: 2021/04/07 18:24:03 by mmassaak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_printable(char *str)
+{
+	int i;
+	int current_letter;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		current_letter = str[i];
+		if (current_letter < 32)
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
