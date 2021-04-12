0999221f181e2e944b8c4d956758f6557b5723f0
mmassaak <mmassaak@student.42sp.org.br>
2021-04-12 13:20:43 +0000

    Add adjusts for norminette sintax

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: mmassaak <mmassaak@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 19:46:45 by mmassaak          #+#    #+#             */
-/*   Updated: 2021/04/06 19:59:13 by mmassaak         ###   ########.fr       */
+/*   Updated: 2021/04/12 12:39:01 by mmassaak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,10 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 	return (dest);
 }
--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: mmassaak <mmassaak@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 18:52:26 by mmassaak          #+#    #+#             */
-/*   Updated: 2021/04/08 19:19:21 by mmassaak         ###   ########.fr       */
+/*   Updated: 2021/04/12 13:20:16 by mmassaak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,17 @@ char	*ft_strcapitalize(char *str)
 	first_letter = 1;
 	while (str[i] != '\0')
 	{
+		if (!first_letter && (str[i] >= 'A' && str[i] <= 'Z'))
+			str[i] = str[i] - ('A' - 'a');
 		if (first_letter)
 		{
 			first_letter = 0;
 			if (str[i] >= 'a' && str[i] <= 'z')
-			{
 				str[i] = str[i] + ('A' - 'a');
-			}
 		}
 		if ((str[i] < 'a' || str[i] > 'z') &&
 			(str[i] < 'A' || str[i] > 'Z'))
-		{
 			first_letter = 1;
-		}
 		i++;
 	}
 	return (str);
